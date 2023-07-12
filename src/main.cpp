51211669a656d97cbb9cc4d5d2d4967acd8e3692
Eduardo Kendy Pontes Veiga <112519484+EduardoKPVeiga@users.noreply.github.com>
2023-07-12 09:21:31 -0300

    Event group example

--- src/main.cpp
+++ src/main.cpp
@@ -7,137 +7,80 @@
 #include <Arduino.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/queue.h"
+#include "freertos/timers.h"
+#include "freertos/event_groups.h"
 
 #define LED 19
 #define BTN 12
+#define TASK1_FLAG  (1<<0) // 0001
+#define TASK2_FLAG  (1<<1) // 0010
 
 /* Handles */
-QueueHandle_t xFila;
-
-TaskHandle_t task1Handle = NULL;
-TaskHandle_t task2Handle = NULL;
-TaskHandle_t task3Handle = NULL;
+TaskHandle_t xTask1Handle = NULL;
+TaskHandle_t xTask2Handle = NULL;
+TimerHandle_t xTimer = NULL;
+EventGroupHandle_t xEventos = NULL;
 
 static bool btnState = false;
 
 /* Protótipo das tasks */
 void vTask1(void *pvParameters);
 void vTask2(void *pvParameters);
-void vTask3(void *pvParameters);
-void trataISR_BT()
-{
-  btnState = !btnState;
-  if (btnState)
-  {
-    vTaskSuspend(task1Handle);
-  }
-  else
-  {
-    vTaskResume(task1Handle);
-  }
-}
+void callBackTimer1(TimerHandle_t pxTimer);
+
+int tempo = 0;
 
 void setup()
 {
-  BaseType_t xReturned;
-
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(LED, OUTPUT);
-  pinMode(BTN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(BTN), trataISR_BT, FALLING);
-
-  xFila = xQueueCreate(5, sizeof(int));
-  if (xFila == NULL)
-  {
-    Serial.println("Não foi possível criar uma fila");
-  }
-
-  xReturned = xTaskCreate(vTask1, "TASK1", configMINIMAL_STACK_SIZE, NULL, 1, &task1Handle);
-  if (xReturned == pdFAIL)
-  {
-    Serial.println("Não foi possível criar a task 1");
-    while (1)
-      ;
-  }
-
-  xReturned = xTaskCreate(vTask2, "TASK2", configMINIMAL_STACK_SIZE + 1024, NULL, 1, &task2Handle);
-  if (xReturned == pdFAIL)
-  {
-    Serial.println("Não foi possível criar a task 2");
-    while (1)
-      ;
-  }
-
-  xReturned = xTaskCreate(vTask3, "TASK3", configMINIMAL_STACK_SIZE + 1024, NULL, 1, &task3Handle);
-  if (xReturned == pdFAIL)
-  {
-    Serial.println("Não foi possível criar a task 3");
-    while (1)
-      ;
-  }
+  xEventos = xEventGroupCreate();
+  xTimer = xTimerCreate("TIMER", pdMS_TO_TICKS(1000), pdTRUE, 0, callBackTimer1);
+  xTaskCreate(vTask1, "TASK1", configMINIMAL_STACK_SIZE + 1024, NULL, 1, &xTask1Handle);
+  xTaskCreate(vTask2, "TASK2", configMINIMAL_STACK_SIZE + 1024, NULL, 1, &xTask2Handle);
+  xTimerStart(xTimer, 0);
 }
 
 void loop()
 {
-  vTaskDelay(3000);
+  digitalWrite(LED, !digitalRead(LED));
+  vTaskDelay(pdMS_TO_TICKS(500));
 }
 
 void vTask1(void *pvParameters)
 {
-  // UBaseType_t uxHighWaterMark;
-  while (1)
+  EventBits_t xBits;
+  while(1)
   {
-    digitalWrite(LED, !digitalRead(LED));
-    vTaskDelay(pdMS_TO_TICKS(200));
-
-    // uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL);
-    // Serial.print(pcTaskGetName(NULL));
-    // Serial.print(": ");
-    // Serial.println(uxHighWaterMark);
+    xBits = xEventGroupWaitBits(xEventos, TASK1_FLAG, pdTRUE, pdTRUE, portMAX_DELAY);
+    Serial.println("Task 1 saiu do bloqueio." + String(xBits));
   }
 }
 
 void vTask2(void *pvParameters)
 {
-  int cont = 0;
-  while (1)
+  EventBits_t xBits;
+  while(1)
   {
-    Serial.print("Cont: ");
-    Serial.println(cont);
-
-    if (cont <= 10)
-    {
-      xQueueSendToBack(xFila, &cont, portMAX_DELAY);
-      cont++;
-    }
-    else
-    {
-      cont = 0;
-      vTaskDelay(pdMS_TO_TICKS(500));
-    }
-    vTaskDelay(pdMS_TO_TICKS(500));
+    xBits = xEventGroupWaitBits(xEventos, TASK2_FLAG, pdTRUE, pdTRUE, portMAX_DELAY);
+    Serial.println("Task 2 saiu do bloqueio." + String(xBits));
   }
 }
 
-void vTask3(void *pvParameters)
+void callBackTimer1(TimerHandle_t pxTimer)
 {
-  int valorRecebido = 0;
-  while (1)
+  tempo++;
+  if (tempo == 5)
   {
-    if (xQueueReceive(xFila, &valorRecebido, pdMS_TO_TICKS(1000)) == pdTRUE)
-    {
-      if (valorRecebido >= 10)
-      {
-        if (task2Handle != NULL)
-        {
-          Serial.println("Suspendendo Tasks");
-          vTaskSuspend(task2Handle);
-          vTaskDelay(pdMS_TO_TICKS(5000));
-          Serial.println("Reiniciando Tasks");
-          vTaskResume(task2Handle);
-        }
-      }
-    }
+    xEventGroupSetBits(xEventos, TASK1_FLAG);
+  }
+  else if (tempo == 10)
+  {
+    xEventGroupSetBits(xEventos, TASK2_FLAG);
+  }
+  else if (tempo == 15)
+  {
+    xEventGroupSetBits(xEventos, TASK1_FLAG | TASK2_FLAG);
+    tempo = 0;
   }
 }
